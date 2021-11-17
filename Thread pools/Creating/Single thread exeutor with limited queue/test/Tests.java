773d284480b99da139139586b8165834ae92bd62
Tomasz Nurkiewicz <tnr@devskiller.com>
2021-11-18 00:02:23 +0100

    Tests for a thread pool with queue

--- Thread pools/Creating/Single thread exeutor with limited queue/test/Tests.java
+++ Thread pools/Creating/Single thread exeutor with limited queue/test/Tests.java
@@ -1,22 +1,16 @@
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
+import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.Semaphore;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class Tests {
+import static org.junit.Assert.assertEquals;
 
-	@Test
-	public void testSingleThread() throws ExecutionException, InterruptedException {
-		ExecutorService executor = new Task().create();
-		Semaphore semaphore = new Semaphore(1);
-		Future<Object> f1 = executor.submit(() -> task(semaphore));
-		Future<Object> f2 = executor.submit(() -> task(semaphore));
-		f1.get();
-		f2.get();
-	}
+public class Tests {
 
 	@Test
 	public void shouldAccept10tasksInQueue() {
@@ -25,7 +19,24 @@ public class Tests {
         for (int i = 0; i <= 10; i++) {
             executor.submit(() -> task(semaphore));
         }
-        Assert.assertEquals(10, executor.shutdownNow().size());
+        assertEquals(10, executor.shutdownNow().size());
+	}
+
+	@Test
+	public void shouldReject11thTask() {
+		ExecutorService executor = new Task().create();
+        Semaphore semaphore = new Semaphore(1);
+        for (int i = 0; i <= 10; i++) {
+            executor.submit(() -> task(semaphore));
+        }
+		try {
+			executor.submit(() -> task(semaphore));
+			Assertions.failBecauseExceptionWasNotThrown(RejectedExecutionException.class);
+		} catch (RejectedExecutionException e) {
+			//expected
+		}
+
+		assertEquals(10, executor.shutdownNow().size());
 	}
 
 	private Object task(Semaphore semaphore) throws InterruptedException {
