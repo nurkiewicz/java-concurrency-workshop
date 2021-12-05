242e07b5cc6cced08613919ac85e1aa056a2d925
Tomasz Nurkiewicz <tnr@devskiller.com>
2021-12-05 13:23:37 +0100

    Clarified

--- CompletableFuture/Combining futures/Wait for first/task.md
+++ CompletableFuture/Combining futures/Wait for first/task.md
@@ -1,8 +1,11 @@
-You have two futures from with the same response.
+You have two futures from with the same response from two different servers.
 One from a server nearby.
 Another from a server on another continent.
-You don't know which is which.
+You don't know which one is which.
 You only care about the very first response.
+
+Create a `CompletableFuture` that completes when the first future completes.
+No matter which one.
 
 <div class="hint">
 
--- CompletableFuture/Combining futures/Wait for first/test/Tests.java
+++ CompletableFuture/Combining futures/Wait for first/test/Tests.java
@@ -11,19 +11,19 @@ public class Tests {
 
 	@Test
 	public void testSolution() throws ExecutionException, InterruptedException, TimeoutException {
-		CompletableFuture<Integer> f = new Task().firstOf(new CompletableFuture<>(), CompletableFuture.completedFuture(1));
+		CompletableFuture<Integer> f = new Task().firstOf(new FutureFailingWhenBlocked<>(), CompletableFuture.completedFuture(1));
 		assertThat(f.get(1, TimeUnit.SECONDS)).isEqualTo(1);
 	}
 
 	@Test
 	public void testSolution2() throws ExecutionException, InterruptedException, TimeoutException {
-		CompletableFuture<Integer> f = new Task().firstOf(CompletableFuture.completedFuture(2), new CompletableFuture<>());
+		CompletableFuture<Integer> f = new Task().firstOf(CompletableFuture.completedFuture(2), new FutureFailingWhenBlocked<>());
 		assertThat(f.get(1, TimeUnit.SECONDS)).isEqualTo(2);
 	}
 
 	@Test
 	public void doesNotBlock() throws ExecutionException, InterruptedException, TimeoutException {
-		CompletableFuture<Integer> f = new Task().firstOf(new CompletableFuture<>(), new CompletableFuture<>());
+		CompletableFuture<Integer> f = new Task().firstOf(new FutureFailingWhenBlocked<>(), new FutureFailingWhenBlocked<>());
 	}
 
 }
