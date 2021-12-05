250f3c80bf4f06107bd1513fad374383a3061049
Tomasz Nurkiewicz <tnr@devskiller.com>
2021-12-05 21:34:12 +0100

    AtomicReference task

--- Data structures/Atomic classes/Arbitrary references/src/Task.java
+++ Data structures/Atomic classes/Arbitrary references/src/Task.java
@@ -1,3 +1,24 @@
-public class Task {
-  //put your task here
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+
+public abstract class Task {
+
+	AtomicReference<Singleton> singleton = new AtomicReference<Singleton>();
+
+	public Singleton getOrCreate() {
+//		return singleton.updateAndGet(cur -> cur == null ? create() : cur);
+		if(singleton.get() == null) {
+			singleton.set(create());
+		}
+		return singleton.get();
+	}
+
+	protected abstract Singleton create();
+
+	public Optional<Singleton> get() {
+		return Optional.ofNullable(singleton.get());
+	}
+
 }
+
+interface Singleton { }
--- Data structures/Atomic classes/Arbitrary references/test/Tests.java
+++ Data structures/Atomic classes/Arbitrary references/test/Tests.java
@@ -1,8 +1,55 @@
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
-public class Tests {
-  @Test
-  public void testSolution() {
-    // put your test here
-  }
+public class Tests implements WithAssertions {
+
+	Singleton instance = new Singleton() {};
+
+	@Test
+	public void smoke() {
+		AtomicInteger invocationCounter = new AtomicInteger();
+		Task task = new Task() {
+			@Override
+			protected Singleton create() {
+				invocationCounter.incrementAndGet();
+				Thread.yield();
+				return instance;
+			}
+		};
+		assertThat(invocationCounter).hasValue(0);
+		assertThat(task.get()).isEmpty();
+
+		assertThat(task.getOrCreate()).isEqualTo(instance);
+		assertThat(invocationCounter).hasValue(1);
+
+		assertThat(task.getOrCreate()).isEqualTo(instance);
+		assertThat(invocationCounter).hasValue(1);
+
+	}
+
+	@RepeatedTest(100)
+	public void multiThreadTest() {
+		AtomicInteger invocationCounter = new AtomicInteger();
+		Task task = new Task() {
+			@Override
+			protected Singleton create() {
+				invocationCounter.incrementAndGet();
+				Thread.yield();
+				return instance;
+			}
+		};
+		assertThat(invocationCounter).hasValue(0);
+		assertThat(task.get()).isEmpty();
+
+		IntStream
+				.range(0, 100)
+				.parallel()
+				.forEach(i -> assertThat(task.getOrCreate()).isEqualTo(instance));
+		assertThat(invocationCounter).hasValue(1);
+
+	}
 }
